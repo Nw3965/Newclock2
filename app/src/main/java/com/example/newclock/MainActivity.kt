a89f54251eff2a2232146d4f8133f9051db5f593
W <www@ssss.com>
2021-08-09 13:21:39 +0900

    test

---
+++ app/src/main/java/com/example/newclock/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.newclock
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import java.util.*
+import kotlin.concurrent.timer
+import android.os.Handler
+import com.example.clock.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+// nameとperiodを指定、{}内が全部actionの指定
+        val handler = Handler( )
+        timer(name = "testTimer", period = 1000) {
+
+            val calendar = Calendar.getInstance()
+            // Set hour, minute and second。
+            val hour = calendar.get(Calendar.HOUR_OF_DAY)
+            val minute = calendar.get(Calendar.MINUTE)
+            val second = calendar.get(Calendar.SECOND)
+            // Send handler to these information below.
+            handler.post {
+                // dateViewのテキストを変更
+                val test = findViewById<TextView>(R.id.textView)
+                "${hour}時${minute}分${second}秒".also { test.text = it }
+
+            }
+
+        }
+    }
+}
